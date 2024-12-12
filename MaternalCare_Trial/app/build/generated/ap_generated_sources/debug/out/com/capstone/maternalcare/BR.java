1f1b9038883fe6b01a748c9d0113d948b79eeea3
margaretham <margarethameiutomo@gmail.com>
2024-12-12 22:50:30 +0700

    Fix code

--- MaternalCare_Trial/app/build/generated/ap_generated_sources/debug/out/com/capstone/maternalcare/BR.java
+++
@@ -1,5 +0,0 @@
-package com.capstone.maternalcare;
-
-public class BR {
-  public static final int _all = 0;
-}
--- MaternalCare_Trial/app/build/generated/data_binding_trigger/debug/com/capstone/maternalcare/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.capstone.maternalcare;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
