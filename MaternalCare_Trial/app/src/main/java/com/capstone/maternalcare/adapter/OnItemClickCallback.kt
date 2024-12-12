40da503d3975d7491e56040ae1321c7b255348c7
margaretham <margarethameiutomo@gmail.com>
2024-12-12 22:37:35 +0700

    Delete MaternalCare_Trial directory

--- MaternalCare_Trial/app/src/main/java/com/capstone/maternalcare/adapter/OnItemClickCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.capstone.maternalcare.adapter
-
-import com.capstone.maternalcare.data.model.Article
-
-interface OnItemClickCallback {
-    fun onItemClicked(data: Article)
-}
--- MaternalCare_Trial/app/src/main/java/com/capstone/maternalcare/ui/profil/ProfilViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.capstone.maternalcare.ui.profil
-
-import com.capstone.maternalcare.data.model.UserPreference
-
-class ProfilViewModel(pref: UserPreference) {
-}
